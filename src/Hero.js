153b40a78e9e0209b8e07632748c8c5705022d8a
yves137 <oneg2981@gmail.com>
2023-10-03 12:12:27 +0200

    update project

--- src/Hero.js
+++
@@ -1,7 +0,0 @@
-export function Hero(){
-    return (
-        <div className="flex justify-center py-20">
-            <img src="photo-grid.png" alt="" className="max-w-[40rem]"/>
-        </div>
-    )
-}
--- src/Navbar.js
+++
@@ -1,7 +0,0 @@
-export function Navbar(){
-    return (
-        <div className="px-10 py-4 shadow-md">
-            <img src="airbnb-logo.png" className="w-40" alt="1"/>
-        </div>
-    )
-}
